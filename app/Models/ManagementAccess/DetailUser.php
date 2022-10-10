5f24f288a7aee5fad42df8f4306c224e2a26519b
adibn22s <adib.shodiq@gmail.com>
2022-10-10 22:07:02 +0700

    create table isnt completed

---
+++ app/Models/ManagementAccess/DetailUser.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\ManagementAccess;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class DetailUser extends Model
+{
+    // use HasFactory;
+    use SoftDeletes;
+}
---
+++ database/migrations/2022_10_10_143826_create_event_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event', function (Blueprint $table) {
+            $table->id();
+            $table->integer('request_event');
+            $table->integer('user_id');
+            $table->string('name')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('event');
+    }
+}
