0a5aff9d0e8b2bb5b342769333c5f51a6027b6af
adibn22s <adib.shodiq@gmail.com>
2022-11-01 20:42:02 +0700

    footer

--- resources/views/components/frontsite/footer.blade.php
+++ resources/views/components/frontsite/footer.blade.php
@@ -1,30 +1,28 @@
 <!-- Footer Start -->
-    <section>
-      <div class="footer-glass">
-        <div class="">
-          <div class="grid grid-cols-3">
-            <div class="footer-image">
-              <div class="footer-image2">
-                <img src="{{ asset('/assets/frontsite/images/about-logo4.png') }}" alt="Gambar komputernya" />
-              </div>
+<footer class="bg-white/30 pt-8 pb-8">
+    <div class="mx-32">
+        <div class="flex justify-between">
+            <div class="w-full px-4 ml-6 mt-10 mb-4 text-slate-300 md:w-1/3">
+                <img src="{{ asset('/assets/frontsite/images/logo_infoevent.png') }}" alt="logo_infoevent" class="h-32 w-44">
             </div>
-            <div class="copyright">
-              <div class="copyright-text">
-                <p>Copyright &#169; Kelompok 2.</p>
-                <p>All rights reserved</p>
-              </div>
+            <div class="w-full px-4 mb-4 ml-20 md:w-1/3">
+                <h3 class="font-semibold text-xl text-white mb-5">Created by</h3>
+                        <span class="inline-block text-base mb-3 text-white">Adib Nasyrul Shodiq</span>
+                        <span class="inline-block text-base mb-3 text-white">Ananda Satria Ariyanto</span>
+                        <span class="inline-block text-base mb-3 text-white">Fernando</span>
+                        <span class="inline-block text-base mb-3 text-white">Muhammad Aulia Nur Fadhillah</span>
+                        <span class="inline-block text-base mb-3 text-white">Vanisa Aulia Putriyani</span>
             </div>
-            <div class="contact-us">
-              <div class="contact-text">
-                <ul class="list-none">
-                  <li>Contact Us</li>
-                  <li>Phone : +62 824-2256-3496</li>
-                  <li>Email : Info-event@event.com</li>
-                </ul>
-              </div>
+            <div class="w-full px-4 mb-4 ml-24 md:w-1/3">
+                <h3 class="font-semibold text-xl text-white mb-5">Contact Us</h3>
+                        <span class="inline-block text-base mb-3 text-white">info-event@event.com</span>
+                        <span class="inline-block text-base mb-3 text-white">+62 81234567890</span>
             </div>
-          </div>
         </div>
-      </div>
-    </section>
-    <!-- Footer End -->
+
+        <div class="w-full pt-6 border-t border-white">
+            <p class="font-medium text-xs text-slate-900 text-center">Created by <span class="font-bold">Kelompok 2 Pemrograman WEB</span>, 3KA01 <span class="text-violet-700">Gunadarma University</span>, with <a href="{{ url('https://tailwindcss.com/') }}" target="_blank" class="font-bold text-sky-600">Tailwind CSS</a>.</p>
+        </div>
+    </div>
+</footer>
+<!-- Footer End -->
